0ed59aa424473b5105d83ab8fc902995e8ad2332
Brandon Fonnemann <branjosef@gmail.com>
2020-08-21 17:38:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,7 +112,7 @@ def changing_alan
     }
 
 programmer_hash[:alan_kay][:known_for] = "GUI"
-programmer_hash
+p programmer_hash
 
 end
 
@@ -135,5 +135,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+p programmer_hash
 
 end
